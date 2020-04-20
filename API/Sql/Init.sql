3f2084d93aa254de44b6fd4aa70f3abf0b9ff094
Geoffroy Millon <geoffroymillon@gmail.com>
2020-04-20 17:30:52 +0200

    Init sql script

---
+++ API/Sql/Init.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE remote_devices
+(
+    id             serial PRIMARY KEY,
+    name           VARCHAR(50)  NOT NULL,
+    hostname       VARCHAR(100) NOT NULL,
+    device_type    VARCHAR(30)  NOT NULL DEFAULT 'LedStrip',
+    min_brightness integer,
+    max_brightness integer
+);
