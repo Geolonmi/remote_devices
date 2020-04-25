41c335af117dc118a8868005983648c1b972742f
Geolonmi <geolonmi@gmail.com>
2020-04-25 09:44:49 +0200

    Make sure everything works

---
+++ ENDPOINT/remote_device_api/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using remote_device_dal;
+
+namespace remote_device_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public TestController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return _appDbContext.RemoteDevices.Select(s => s.Id);
+        }
+    }
+}
